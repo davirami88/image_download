9046dafe99c942f0aa783e67f3939feef34d36be
David Ramírez <davirami88@gmail.com>
2023-11-03 11:46:15 -0500

    Update script in order to avoid errors

--- download_images.py
+++ download_images.py
@@ -5,23 +5,29 @@ Created on Sun Jun 25 15:49:32 2017
 @author: davirami
 """
 from icrawler.builtin import GoogleImageCrawler
-import numpy as np
 import os
+import random
 
 path = 'Path/to/Save/Photos/'
 keyword='your query to search images'
 
-google_crawler = GoogleImageCrawler(parser_threads=50, downloader_threads=500,
+google_crawler = GoogleImageCrawler(parser_threads=2, downloader_threads=4,
                                     storage={'root_dir': path})
-google_crawler.crawl(keyword=keyword, max_num=1000,
-                     date_min=None, date_max=None,
-                     min_size=(400, 200), max_size=None)
+
+try:
+    google_crawler.crawl(keyword=keyword, max_num=100,
+                         min_size=(400, 200), max_size=None)
+except Exception as e:
+    print(f"An error occurred: {e}")
 
 files = os.listdir(path)
-i = 1
-
 for file in files:
-    #changes the names of the different pictures and transform them to .jpg
-    os.rename(os.path.join(path, file), os.path.join(path, str(np.random.randint(2000000))+'.jpg'))
-    i = i+1
-    print(str(np.random.randint(2000000)))
+    new_filename = str(random.randint(2000000, 2999999)) + '.jpg'
+    new_file_path = os.path.join(path, new_filename)
+    
+    while os.path.exists(new_file_path):
+        new_filename = str(random.randint(2000000, 2999999)) + '.jpg'
+        new_file_path = os.path.join(path, new_filename)
+    
+    os.rename(os.path.join(path, file), new_file_path)
+    print(new_filename)
